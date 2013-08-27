ec0a13bae5bed173377eb4092391805e9ff77d58
HPNeo <gustavo.leon@outlook.com>
2013-08-27 18:15:30 -0500

    Refactoring

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,37 +1,27 @@
 Uatu.on('after_tab_change', function(tab_page) {
   switch (tab_page) {
     case 'docentes' :
-      var docentes = UPC.models.Docente.all();
-      docentes.sort(function(a, b) {
+      var collection = UPC.models.Docente.all();
+      collection.sort(function(a, b) {
         return a.get('nombre').localeCompare(b.get('nombre'));
       });
-
-      var template_list_item = $('#template_list_item');
-      $('.list').html('');
-
-      for (var i = 0; i < docentes.length; i++) {
-        var docente = docentes[i];
-        var template = template_list_item.html();
-        var content = template.replace('{{nombre}}', docente.get('nombre'));
-        $('.list').append(content);
-      }
     break;
     case 'cursos' :
-      var cursos = UPC.models.Curso.all();
-      cursos.sort(function(a, b) {
+      var collection = UPC.models.Curso.all();
+      collection.sort(function(a, b) {
         return a.get('nombre').localeCompare(b.get('nombre'));
       });
-
-      var template_list_item = $('#template_list_item');
-      $('.list').html('');
-
-      for (var i = 0; i < cursos.length; i++) {
-        var curso = cursos[i];
-        var template = template_list_item.html();
-        var content = template.replace('{{nombre}}', curso.get('nombre'));
-        $('.list').append(content);
-      }
     break;
+  }
+
+  var template_list_item = $('#template_list_item');
+  $('.list').html('');
+
+  for (var i = 0; i < collection.length; i++) {
+    var item = collection[i];
+    var template = template_list_item.html();
+    var content = template.replace('{{nombre}}', item.get('nombre'));
+    $('.list').append(content);
   }
 });
 
