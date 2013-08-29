8a1fcd8fc97c34bd3c0ddf1c13a0ccfc2e3dbdee
HPNeo <hpneo@hotmail.com>
2013-08-29 17:04:16 -0500

    Add timetable

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,3 +1,35 @@
+var translate3d = function(element, x, y, z) {
+  element.css('-webkit-transform', 'translate3d(' + x +',' + y + ', ' + z + ')');
+  element.css('-moz-transform', 'translate3d(' + x +',' + y + ', ' + z + ')');
+  element.css('-ms-transform', 'translate3d(' + x +',' + y + ', ' + z + ')');
+  element.css('-o-transform', 'translate3d(' + x +',' + y + ', ' + z + ')');
+  element.css('transform', 'translate3d(' + x +',' + y + ', ' + z + ')');
+}
+
+var swapContent = function(previous, next) {
+  translate3d(previous, '0%', 0, 0);
+  translate3d(previous.addClass('slide'), '-100%', 0, 0);
+
+  $('.content').prepend(next.addClass('slide'));
+  translate3d(next, '100%', 0, 0);
+  window.setTimeout(function() {
+    translate3d(next, '0%', 0, 0);
+
+    window.setTimeout(function() {
+      previous.remove();
+    }, 251);
+  }, 25);
+}
+
+var timeFormatter = function(time) {
+  if (time > 12) {
+    return (time - 12) + " PM";
+  }
+  else {
+    return time + " AM";
+  }
+}
+
 Uatu.on('after_tab_change', function(tab_page, collection) {
   switch (tab_page) {
     case 'docentes' :
@@ -19,30 +51,58 @@ Uatu.on('after_tab_change', function(tab_page, collection) {
   });
 });
 
-var translate3d = function(element, x, y, z) {
-  element.css('-webkit-transform', 'translate3d(' + x +',' + y + ', ' + z + ')');
-  element.css('-moz-transform', 'translate3d(' + x +',' + y + ', ' + z + ')');
-  element.css('-ms-transform', 'translate3d(' + x +',' + y + ', ' + z + ')');
-  element.css('-o-transform', 'translate3d(' + x +',' + y + ', ' + z + ')');
-  element.css('transform', 'translate3d(' + x +',' + y + ', ' + z + ')');
-}
+Uatu.on('after_click_list_item', function(collection_type, model, collection) {
+  if (collection_type == 'secciones') {
+    UPC.views.loadTemplate(collection_type, function(content) {
+      var content = UPC.views.populateTemplate(content, model);
+      content = $(content);
 
-Uatu.on('before_swap', function(collection_type, model, collection) {
-  UPC.views.loadTemplate('simple_list', function(parent_content) {
-    UPC.views.loadTemplate('list_item_section', function(content) {
-      var content = UPC.views.populateTemplate(content, collection);
-      content = $(parent_content).filter('.list').attr('data-collection', 'secciones').append(content);
-      
-      translate3d($('.content').children().first(), '0%', 0, 0);
-      translate3d($('.content').children().first().addClass('slide'), '-100%', 0, 0);
+      var horario = model.get('horario');
 
-      $('.content').prepend(content.addClass('slide'));
-      translate3d(content, '100%', 0, 0);
-      window.setTimeout(function() {
-        translate3d(content, '0%', 0, 0);
-      }, 25);
+      for(d in horario) {
+        var hours = horario[d];
+        var hours_list = content.find('.list#' + d);
+
+        hours_list.append('<li>' + timeFormatter(hours[0]) + ' - ' + timeFormatter(hours[1]) + '</li>');
+      }
+
+      swapContent($('.content').children().first(), content);
     });
-  });
+  }
+  else {
+    UPC.views.loadTemplate('simple_list', function(parent_content) {
+      UPC.views.loadTemplate('list_item_section', function(content) {
+        var content = UPC.views.populateTemplate(content, collection);
+        content = $(parent_content).filter('.list').attr('data-collection', 'secciones').append(content);
+        
+        swapContent($('.content').children().first(), content);
+      });
+    });
+  }
+});
+
+$(document).on('click', '.list-item a', function(e) {
+  e.preventDefault();
+
+  var collection_type = $(this).parents('.list').data('collection'),
+      id = $(this).parent().data('id');
+
+  switch (collection_type) {
+    case 'docentes':
+      var model = UPC.models.Docente.find(id);
+      var collection = model.get('secciones');
+    break;
+    case 'cursos':
+      var model = UPC.models.Curso.find(id);
+      var collection = model.get('secciones');
+    break;
+    case 'secciones':
+      var model = UPC.models.Seccion.find(id);
+      collection = [];
+    break;
+  }
+
+  Uatu.fire('after_click_list_item', null, collection_type, model, collection);
 });
 
 $(document).on('click', '.tab-item a', function(e) {
@@ -73,26 +133,18 @@ $(document).on('click', '.tab-item a', function(e) {
   Uatu.fire('after_tab_change', null, tab_page, collection || []);
 });
 
-$(document).on('click', '.list-item a', function(e) {
+$(document).on('click', '.segmented-controller a', function(e) {
   e.preventDefault();
 
-  var collection_type = $(this).parents('.list').data('collection'),
-      id = $(this).parent().data('id');
+  var segmented_tab = $(this).parent();
+  var segmented_controller = segmented_tab.parent();
+  var segmented_container = segmented_controller.parent();
+  var segmented_tab_id = $(this).attr('href');
+  var segmented_list = segmented_container.find('.list' + segmented_tab_id);
 
-  switch (collection_type) {
-    case 'docentes':
-      var model = UPC.models.Docente.find(id);
-      var collection = model.get('secciones');
-    break;
-    case 'cursos':
-      var model = UPC.models.Curso.find(id);
-      var collection = model.get('secciones');
-    break;
-    case 'secciones':
-      var model = UPC.models.Seccion.find(id);
-      collection = [];
-    break;
-  }
+  segmented_controller.children('li').removeClass('active');
+  segmented_tab.addClass('active');
 
-  Uatu.fire('before_swap', null, collection_type, model, collection);
+  segmented_container.find('.list').hide();
+  segmented_list.show();
 });
